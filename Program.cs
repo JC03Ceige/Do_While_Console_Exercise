d28b912e54dae70d7dc991a4e5a6f1fdb961a85d
jcvdmerwe03@gmail.com <jcvdmerwe03@gmail.com>
2020-11-20 14:00:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Exercises_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            
+            do
+            {
+                userInput = GetUserInput();
+                if (userInput.Length < 5)
+                {
+                    Console.WriteLine("You must enter at least 5 characters.");
+                }
+            }
+            while (userInput.Length < 5);
+                        
+        }
+
+        public static string GetUserInput()
+        {
+            Console.WriteLine("Please type something...");
+            return Console.ReadLine();
+        }
+    }
+}
